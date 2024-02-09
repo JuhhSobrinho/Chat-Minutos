34c774a6c491e1c65712e096aeca962e745efb8b
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2024-02-09 16:53:09 -0300

    utilizando server.js e websocket

--- functions/server/server.js
+++ functions/server/server.js
@@ -1,31 +1,35 @@
-const { Server } = require('ws');
+const { Server: WebSocketServer } = require('ws');
 
 let wss;
+
+const startWebSocket = () => {
+  wss = new WebSocketServer({ noServer: true });
+
+  wss.on('connection', (ws) => {
+    console.log('Cliente conectado via WebSocket');
+
+    ws.on('message', (message) => {
+      console.log(`Mensagem recebida via WebSocket: ${message}`);
+      // Adapte conforme necessário para enviar a mensagem para o Socket.IO
+    });
+  });
+
+  wss.httpServer.on('request', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
+    res.end('WebSocket servidor ativo.');
+  });
+
+  wss.on('close', () => {
+    console.log('Conexão WebSocket fechada');
+    wss = null;
+  });
+};
 
 exports.handler = async (event) => {
   const { request, socket, head } = event;
 
   if (!wss) {
-    wss = new Server({ noServer: true });
-
-    wss.on('connection', (ws) => {
-      console.log('Cliente conectado via WebSocket');
-
-      ws.on('message', (message) => {
-        console.log(`Mensagem recebida via WebSocket: ${message}`);
-        // Adapte conforme necessário para enviar a mensagem para o Socket.IO
-      });
-    });
-
-    wss.httpServer.on('request', (req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
-      res.end('WebSocket servidor ativo.');
-    });
-
-    wss.on('close', () => {
-      console.log('Conexão WebSocket fechada');
-      wss = null;
-    });
+    startWebSocket();
 
     wss.handleUpgrade(request, socket, head, (ws) => {
       wss.emit('connection', ws, request);
